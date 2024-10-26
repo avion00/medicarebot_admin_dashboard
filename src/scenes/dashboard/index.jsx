62b1a403aa4384da7a8ef231c2294a85c78db91a
Abhishek kumar chaudhary <avioncompany00@gmail.com>
2024-10-26 17:34:38 +0545

    training bot

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,22 +1,14 @@
 import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
 import { tokens } from "../../theme";
-// import { mockTransactions } from "../../data/mockData";
 import { activeUser } from "../../data/activeUser";
 import { botDeployed } from "../../data/botDeployed";
 import { recentActivities } from "../../data/recentActivities";
 
 import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
-// import EmailIcon from "@mui/icons-material/Email";
-// import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
-// import PersonAddIcon from "@mui/icons-material/PersonAdd";
-// import TrafficIcon from "@mui/icons-material/Traffic";
 import Header from "../../components/Header";
 import LineChart from "../../components/LineChart";
-// import GeographyChart from "../../components/GeographyChart";
 import Table from "../../components/table";
 
-// import BarChart from "../../components/BarChart";
-// import StatBox from "../../components/StatBox";
 import ProgressCircle from "../../components/ProgressCircle";
 
 const Dashboard = () => {
@@ -25,7 +17,6 @@ const Dashboard = () => {
 
   return (
     <Box m="20px">
-      {/* HEADER */}
       <Box
         display="flex"
         justifyContent="space-between"
